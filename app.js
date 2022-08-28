138b58e27ec9e887f9470e21babb9d2e6f8ae166
davidS <davidsiregar699@gmail.com>
2022-08-28 17:35:36 +0700

    1.0.1

--- app.js
+++ app.js
@@ -1,47 +1,73 @@
-const express = require("express")
-const https= require("https")
-const bodyParser = require("body-parser")
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const { read } = require("fs");
 
-const app=express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname+ "/index.html")
-})
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
 
-app.post("/", function(req, res){
-  const query=req.body.inputBox
-  const units="metric"
-  const apiKey="a2162b02c36ddfbdb35334383aa664e9"
-  const url= "https://api.openweathermap.org/data/2.5/weather?q="+ query+"&appid="+apiKey+"&units="+units
+app.use(express.static(__dirname + "/public"));
 
-https.get(url, function(response){
-  console.log(response.statusCode);
+app.post("/", function (req, res) {
+	const query = req.body.inputBox;
+	const units = "metric";
+	const apiKey = "a2162b02c36ddfbdb35334383aa664e9";
+	const url =
+		"https://api.openweathermap.org/data/2.5/weather?q=" +
+		query +
+		"&appid=" +
+		apiKey +
+		"&units=" +
+		units;
 
-  response.on("data", function(data){
-    const weatherData=JSON.parse(data)
-    // When receiving data from a web server, the data is always a string. =json
-    const temp =weatherData.main.temp;
-    const weatherDesc=weatherData.weather[0].description
-    const icon=weatherData.weather[0].icon
-    const imageUrl="http://openweathermap.org/img/wn/" + icon + "@2x.png"
-    res.write('<div class="container_output">')
-    res.write("<p>The weather is currently " + weatherDesc + "</p>")
-    res.write(
-      "<h1>the temperature in" + query + " is " + temp + " degree celsius "+ "</h1>"
-    )
-    res.write("<img src=" + imageUrl + ">")
-      res.write("</div>")
-    res.send()
-  })
-})
-})
+	https.get(url, function (response) {
+		console.log(response.statusCode);
 
-app.listen(3004, function(){
-  console.log("server is running");
-})
+		response.on("data", function (data) {
+			const weatherData = JSON.parse(data);
+			// When receiving data from a web server, the data is always a string. =json
+			const temp = weatherData.main.temp;
+			// const tempMax = weatherData.main.temp_max;
+			const weatherDesc = weatherData.weather[0].description;
+			const icon = weatherData.weather[0].icon;
+			const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-app.post("/", function(req,res){
-  console.log("post request received");
-})
+			res.write('<div style="border: 2px dotted">');
+			res.write(
+				'<h1 style="color:grey;text-align:center; margin-top: 10rem;line-height: 5;border: 3px solid dotted;">'
+			);
+			res.write("<p>The weather is currently " + weatherDesc + "</p>");
+			res.write(
+				'<h2 style="color:red;text-align:center;">the temperature in ' +
+					query +
+					" is " +
+					temp +
+					" degree celsius " +
+					"</h2>"
+			);
+
+			res.write(
+				' <img style="margin-left: 45rem;border: 1px solid; width: 7rem; "  src=' +
+					imageUrl +
+					"> "
+			);
+
+			res.write("</h1>");
+			res.write("</div>");
+			res.send();
+		});
+	});
+});
+
+app.listen(3004, function () {
+	console.log("server is running");
+});
+
+app.post("/", function (req, res) {
+	console.log("post request received");
+});
